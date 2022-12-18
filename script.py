e7149bf6fec70567d6426d78442d5d69e8b6d53e
sohamganatra <sohamganatra1@gmail.com>
2022-12-18 15:25:56 +0530

    Removed test files

--- script.py
+++
@@ -1,8 +0,0 @@
-from builtins import open
-from sources.models import Source
-
-with open('datalist.txt', 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        source = Source(link=line.strip(), type="html")
-        source.save()
